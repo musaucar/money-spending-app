daa658a6eec9b75554541332cefc99b05d5b7a1f
Musa Uçar <92794324+musaucar@users.noreply.github.com>
2022-10-08 20:34:57 +0300

    money spending app update

--- src/App.js
+++ src/App.js
@@ -7,12 +7,17 @@ import products from './products.json'
 function App() {
 
   const [money, setMoney] = useState(100);
+  const [basket, setBasket] = useState([])
+
+  useEffect(() =>{
+    console.log(basket)
+  }, [basket])
 
   return (
     <>
       <Header money={money}/>
       {products.map(product => (
-        <Product product={product}/>
+        <Product key={product.id} basket={basket} setBasket={setBasket} product={product}/>
       ))}
     </>
   );
